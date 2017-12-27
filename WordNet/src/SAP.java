ff15e8894a0e7960f0e1915364e6a07f2b6bc5ae
Anton Gurkovsky <gurkovsky@ukr.net>
2017-12-28 00:06:34 +0200

    SAP implementation BFS for common ancestor has been started... need to complete

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -1,9 +1,14 @@
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
+	private boolean[] marked;  // marked[v] = is there an s->v path?
+    private int[] edgeTo;      // edgeTo[v] = last edge on shortest s->v path
+    private int[] distTo;      // distTo[v] = length of shortest s->v path
+    
 	/**
 	 * constructor takes a digraph (not necessarily a DAG)
 	 */
@@ -40,6 +45,43 @@ public class SAP {
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
 		throw new RuntimeException();
 	}
+	
+	// BFS from single source
+    private void bfs(Digraph G, int s1, int s2) {
+        Queue<Integer> q1 = new Queue<Integer>();
+        Queue<Integer> q2 = new Queue<Integer>();
+        marked[s1] = true;
+        distTo[s1] = 0;
+        q1.enqueue(s1);
+        
+        marked[s2] = true;
+        distTo[s2] = 0;
+        q2.enqueue(s2);
+        
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            int v1 = q1.dequeue();
+            int v2 = q2.dequeue();
+            
+            if (v1 == v2) break;
+            
+            for (int w1 : G.adj(v1)) {
+                if (!marked[w1]) {
+                    edgeTo[w1] = v1;
+                    distTo[w1] = distTo[v1] + 1;
+                    marked[w1] = true;
+                    q1.enqueue(w1);
+                }
+            }
+            for (int w2 : G.adj(v2)) {
+                if (!marked[w2]) {
+                    edgeTo[w2] = v2;
+                    distTo[w2] = distTo[v2] + 1;
+                    marked[w2] = true;
+                    q1.enqueue(w2);
+                }
+            }
+        }
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
