8d339f356bb50b92bb5e8971bb0f07992b09bfe5
Anton Gurkovsky <gurkovsky@ukr.net>
2018-01-23 01:17:47 +0200

    An implementation of SAP with iterable args has been added. Class WordNet has been completed

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -1,4 +1,6 @@
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import edu.princeton.cs.algs4.Digraph;
@@ -8,27 +10,23 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
-	private boolean[] marked1; // marked[v] = is there an s->v path?
-	private int[] edgeTo1; // edgeTo[v] = last edge on shortest s->v path
-	private int[] distTo1; // distTo[v] = length of shortest s->v path
 	
-	private boolean[] marked2;
-	private int[] edgeTo2;
-	private int[] distTo2;
-	
-	private Set<Integer> vertices;
+	private final Map<String, BFS> pull = new HashMap<>();
+	private final Digraph graph;
 	
 	/**
 	 * constructor takes a digraph (not necessarily a DAG)
 	 */
 	public SAP(Digraph G) {
+		graph = new Digraph(G);
+		
 	}
 
 	/**
 	 * length of shortest ancestral path between v and w; -1 if no such path
 	 */
 	public int length(int v, int w) {
-		throw new RuntimeException();
+		return retrieveInstance(v, w).getLength();
 	}
 
 	/**
@@ -36,7 +34,7 @@ public class SAP {
 	 * path; -1 if no such path
 	 */
 	public int ancestor(int v, int w) {
-		throw new RuntimeException();
+		return retrieveInstance(v, w).getAncestor();
 	}
 
 	/**
@@ -44,7 +42,7 @@ public class SAP {
 	 * in w; -1 if no such path
 	 */
 	public int length(Iterable<Integer> v, Iterable<Integer> w) {
-		throw new RuntimeException();
+		return retrieveInstance(v, w).getLength();
 	}
 
 	/**
@@ -52,58 +50,12 @@ public class SAP {
 	 * such path
 	 */
 	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-		throw new RuntimeException();
+		return retrieveInstance(v, w).getAncestor();
 	}
 
-	// BFS from single source
-	private void bfs(Digraph G, int s1, int s2) {
-		Queue<Integer> q1 = new Queue<Integer>();
-		Queue<Integer> q2 = new Queue<Integer>();
-		marked1[s1] = true;
-		distTo1[s1] = 0;
-		q1.enqueue(s1);
+	
 
-		marked2[s2] = true;
-		distTo2[s2] = 0;
-		q2.enqueue(s2);
-		
-		vertices = new HashSet<>();
-		boolean pathFound = false;
-
-		while (!q1.isEmpty() && !q2.isEmpty() && !pathFound) {
-			int v1 = q1.dequeue();
-			int v2 = q2.dequeue();
-
-			pathFound = walkThroughAdjustedVertices(G, q1, v1, marked1, edgeTo1, distTo1);
-			if (!pathFound) pathFound = walkThroughAdjustedVertices(G, q2, v2, marked2, edgeTo2, distTo2);
-		}
-	}
-
-	/**
-	 * 
-	 * @param G
-	 * @param q
-	 * @param v
-	 * @param marked
-	 * @param edgeTo
-	 * @param distTo
-	 * @return
-	 */
-	private boolean walkThroughAdjustedVertices(
-			Digraph G, Queue<Integer> q, int v, boolean[] marked, int[] edgeTo, int[] distTo) {
-		boolean pathFound = false;
-		for (int w : G.adj(v)) {
-			if (!marked[w]) {
-				edgeTo[w] = v;
-				distTo[w] = distTo[v] + 1;
-				marked[w] = true;
-				q.enqueue(w);
-				if (vertices.contains(w)) pathFound = true;
-				else vertices.add(w);
-			}
-		}
-		return pathFound;
-	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -118,5 +70,113 @@ public class SAP {
 			StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
 		}
 	}
+	
+	private BFS retrieveInstance(int v, int w) {
+		String key;
+		if (v < w) key = Integer.toString(v) + Integer.toString(w);
+		else key = Integer.toString(w) + Integer.toString(v);
+		if (!pull.containsKey(key)) pull.put(key, new BFS(v, w));
+		
+		return pull.get(key);
+	}
+	
+	private BFS retrieveInstance(Iterable<Integer>vs, Iterable<Integer>ws) {
+		String key;
+		if (vs.hashCode() < ws.hashCode())key = Integer.toString(vs.hashCode()) + Integer.toString(ws.hashCode());
+		else key = Integer.toString(ws.hashCode()) + Integer.toString(vs.hashCode());
+		if (!pull.containsKey(key)) pull.put(key, new BFS(vs, ws));
+		
+		return pull.get(key);
+	}
+	
+	private class BFS {
+ 
+		boolean[] marked1 = new boolean[graph.V()];// marked[v] = is there an s->v path?
+		int[] distTo1 = new int[graph.V()];// edgeTo[v] = last edge on shortest s->v path
+		int[] edgeTo1 = new int[graph.V()];// distTo[v] = length of shortest s->v path
+		Queue<Integer> q1 = new Queue<Integer>();
+		
+		boolean[] marked2 = new boolean[graph.V()];
+		int[] distTo2 = new int[graph.V()];
+		int[] edgeTo2 = new int[graph.V()];
+		Queue<Integer> q2 = new Queue<Integer>();
+		
+		final int ancestor;
+		
+		final Set<Integer> vertices = new HashSet<>();
+		
+		BFS (int v, int w) {
+	        marked1[v] = true;
+			distTo1[v] = 0;
+			q1.enqueue(v);
+			
+	        
+	        marked2[w] = true;
+			distTo2[w] = 0;
+			q2.enqueue(w);
+			
+			ancestor = searchOfShortestPath();
+		}
+		
+		BFS (Iterable<Integer> vs, Iterable<Integer> ws) {
+			for (int v : vs) {
+				marked1[v] = true;
+				distTo1[v] = 0;
+				q1.enqueue(v);
+			}
+	        
+			for (int w : ws) {
+				marked2[w] = true;
+				distTo2[w] = 0;
+				q2.enqueue(w);
+			}
+			
+			ancestor = searchOfShortestPath();
+		}
+		
+		int getAncestor() {
+			return ancestor;
+		}
+		
+		int getLength() {
+			return vertices.size();
+		}
+		
+		int searchOfShortestPath() {
+			int ancestor = -1;
 
+			while (!q1.isEmpty() && !q2.isEmpty() && -1 == ancestor) {
+				int v1 = q1.dequeue();
+				int v2 = q2.dequeue();
+
+				ancestor = findShortestPath(q1, v1, marked1, edgeTo1, distTo1);
+				if (-1 == ancestor) ancestor = findShortestPath(q2, v2, marked2, edgeTo2, distTo2);
+			}
+			return ancestor;
+		}
+		
+		/**
+		 * 
+		 * @param q
+		 * @param v
+		 * @param marked
+		 * @param edgeTo
+		 * @param distTo
+		 * @return
+		 */
+		int findShortestPath(Queue<Integer> q, int v, boolean[] marked, int[] edgeTo, int[] distTo) {
+			int pathFound = -1;
+			for (int w : graph.adj(v)) {
+				if (!marked[w]) {
+					edgeTo[w] = v;
+					distTo[w] = distTo[v] + 1;
+					marked[w] = true;
+					q.enqueue(w);
+					if (vertices.contains(w)) pathFound = w;
+					else vertices.add(w);
+				}
+			}
+			return pathFound;
+		}
+	}
 }
--- WordNet/src/WordNet.java
+++ WordNet/src/WordNet.java
@@ -9,54 +9,60 @@ import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 
-
 public class WordNet {
 
 	private final int V;
 	private final Digraph DG;
-	private final Map<String, List<Integer>> mapper;
-	
-	/** 
-	 * constructor takes the name of the two input files 
+	private final Map<String, List<Integer>> mapper = new HashMap<>();
+	private final Map<Integer, String> mapperReverced = new HashMap<>();
+	private final SAP sap;
+
+	/**
+	 * constructor takes the name of the two input files
 	 */
 	public WordNet(String synsets, String hypernyms) {
-		
+
 		String contentSynset = new In(synsets).readAll();
 		String contentHypernym = new In(hypernyms).readAll();
-		
+
 		StringTokenizer lineTokensSynset = new StringTokenizer(contentSynset);
 		StringTokenizer lineTokensHypernym = new StringTokenizer(contentHypernym);
-		
+
 		V = lineTokensSynset.countTokens();
 		DG = new Digraph(V);
-		mapper = new HashMap<String, List<Integer>>();
-		if (V != lineTokensHypernym.countTokens()) throw new IllegalArgumentException();
-		
+		if (V != lineTokensHypernym.countTokens())
+			throw new IllegalArgumentException();
+
 		while (lineTokensSynset.hasMoreTokens()) {
-			
+
 			String lineSynset = lineTokensSynset.nextToken();
 			String lineHypernym = lineTokensHypernym.nextToken();
-			
+
 			StringTokenizer tokenizerSynset = new StringTokenizer(lineSynset, ",");
 			StringTokenizer tokenizerHypernym = new StringTokenizer(lineHypernym, ",");
-			
+
 			int id = Integer.parseInt(tokenizerSynset.nextToken());
 			int _id = Integer.parseInt(tokenizerHypernym.nextToken());
-			
-			if (id != _id) throw new IllegalArgumentException();
-			
+
+			if (id != _id)
+				throw new IllegalArgumentException();
+
 			StringTokenizer wordTokenizer = new StringTokenizer(tokenizerSynset.nextToken());
 			while (wordTokenizer.hasMoreTokens()) {
 				String word = wordTokenizer.nextToken();
-				if (mapper.containsKey(word)) mapper.get(word).add(id);
-				else mapper.put(word, Arrays.asList(id));
+				mapperReverced.put(id, word);
+				if (mapper.containsKey(word))
+					mapper.get(word).add(id);
+				else
+					mapper.put(word, Arrays.asList(id));
 			}
 			StringTokenizer hypernymTokenizer = new StringTokenizer(tokenizerHypernym.nextToken());
 			while (hypernymTokenizer.hasMoreTokens()) {
 				int hypernym = Integer.parseInt(hypernymTokenizer.nextToken());
 				DG.addEdge(id, hypernym);
 			}
-	     }
+		}
+		sap = new SAP(DG);
 	}
 
 	/**
@@ -66,18 +72,18 @@ public class WordNet {
 		return mapper.keySet();
 	}
 
-	/** 
-	 * is the word a WordNet noun? 
+	/**
+	 * is the word a WordNet noun?
 	 */
 	public boolean isNoun(String word) {
 		return mapper.containsKey(word);
 	}
 
-	/** 
+	/**
 	 * distance between nounA and nounB (defined below)
 	 */
 	public int distance(String nounA, String nounB) {
-		throw new RuntimeException();
+		return sap.length(mapper.get(nounA), mapper.get(nounB));
 	}
 
 	/**
@@ -85,39 +91,11 @@ public class WordNet {
 	 * nounA and nounB in a shortest ancestral path (defined below)
 	 */
 	public String sap(String nounA, String nounB) {
-		throw new RuntimeException();
+		return mapperReverced.get(sap.ancestor(mapper.get(nounA), mapper.get(nounB)));
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
-	private class Node {
-		int synsetId;
-		String[] synset;
-		int hash;
-		
-		@Override
-		public int hashCode() {
-			int hashCode = hash;
-			if (hashCode == 0) {
-				for(int i = 0; i < synset.length; i++){
-					hashCode = 31*hashCode + synset[i].hashCode();
-				}
-				hash = hashCode;
-			}
-			return hashCode;
-		}
-		
-		@Override
-		public boolean equals(Object that) {
-			if (that == null) return false;
-			if (this.getClass() != that.getClass()) return false;
-			Node thatNode =  (Node)that;
-			if (hashCode() == thatNode.hashCode() && synsetId == thatNode.synsetId) return true;
-			return false;
-		}
-	}
-
 }
